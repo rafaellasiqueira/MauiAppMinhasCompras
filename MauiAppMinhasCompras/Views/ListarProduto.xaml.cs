5264649d8f33e6a899244de5f08775947725a4c1
55119 <55119@DESKTOP-9018EBD>
2025-03-15 19:56:54 -0300

    Implementação da Tabela

--- MauiAppMinhasCompras/Views/ListarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListarProduto.xaml.cs
@@ -1,11 +1,27 @@
+using System.Collections.ObjectModel;
+
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListarProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
+
 	public ListarProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
+
+    protected async override void OnAppearing()
+    {
+		List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach(i => lista.Add(i));
+    }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
@@ -17,5 +33,30 @@ public partial class ListarProduto : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "Ok");
 		}
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O Total é {soma:C}";
+
+		DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
     }
 }
